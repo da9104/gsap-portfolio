67f1fb5e162df1f96b9aabdf89cf267e594be1a0
Dami <dmk9104@gmail.com>
2024-07-12 21:58:19 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,214 @@
+document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger)
+
+    gsap.set("#landing-img", {
+        scale: 0.8
+    })
+    gsap.set("#landing-img2", {
+        scale: 0.8
+    })
+    gsap.set("#landing-img0", {
+        scale: 0.8
+    })
+    gsap.set("#landing-img3", {
+        scale: 0.8
+    })
+
+    gsap.timeline()
+        .to("#landing-img3", {
+            // opacity: 1,
+            x: '-60px',
+            duration: 1,
+            rotation: "-1deg",
+        })
+        .to("#landing-img", {
+            rotation: "1deg",
+        })
+        .to("#landing-img2", {
+            x: '170px',
+            rotation: "3deg",
+        })
+        .to("#landing-img0", {
+            x: '-10px',
+            duration: 1,
+            rotation: "-3deg",
+        })
+       
+    
+    gsap.to(".menu", {
+        y: "-50%",
+        opacity: 0,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "top top",
+            end: "50vh top",
+            scrub: 1,
+        }
+    })    
+
+    gsap.to("#logo", {
+        y: "-50%",
+        scale: 0.35,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "top top",
+            end: "50vh top",
+            scrub: 1,
+        }
+    })
+    gsap.to("#logo", {
+        filter: "blur(10px)",
+        scale: 0,
+        y: '-100%',
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "top top",
+            end: "120vh top",
+            scrub: 1,
+        }
+    })
+
+    gsap.to(".dashboard", {
+        y: "-35%",
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "top top",
+            end: "800vh top",
+            scrub: 1,
+        }
+    })
+
+
+    gsap.to("#landing-img3", {
+        filter: "blur(10px)",
+        y: '70px',
+        x: '-200px',
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "top top",
+            end: "140vh top",
+            scrub: 0.5,
+        }
+    })
+
+    gsap.to("#landing-img", {
+        filter: "blur(10px)",
+        y: '-60px',
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "200vh top",
+            end: "800vh top",
+            scrub: 0.5,
+        }
+    })
+
+    gsap.to("#landing-img2", {
+        filter: "blur(10px)",
+        x: '230px',
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "450vh top",
+            end: "800vh top",
+            scrub: 0.5,
+        }
+    })
+
+    gsap.to("#window img", {
+        opacity: 0,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "20vh top",
+            end: "450vh top",
+            scrub: 0.5,
+        }
+    })
+
+    gsap.to("#landing-img0", {
+        filter: "blur(10px)",
+        y: '-100px',
+        x: '-200px',
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "360vh top",
+            end: "950vh top",
+            scrub: 1,
+        }
+    })
+
+    gsap.to(".dashboard", {
+        opacity: 0,
+        display: "none",
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "600vh top",
+            end: "825vh top",
+            scrub: true,
+        }
+    })
+
+    gsap.from('.marquee', 2, {
+        // top: '100vh',
+        ease: 'power3.inOut',
+        delay: 0.9,
+    })
+    
+    gsap.to(".marquee", {
+        opacity: 0,
+        filter: "blur(10px)",
+        ease: "power3.inOut",
+        scrollTrigger: {
+            trigger: ".dashboard",
+            start: "680vh top",
+            end: "2025vh top",
+            scrub: true,
+        }
+    })
+
+    gsap.set(".features", {
+        filter: "blur(10px)",
+    })
+
+    gsap.to(".features", {
+        filter: 'none',
+        ease: "power3.inOut",
+        scrollTrigger: {
+            trigger: ".web-content",
+            start: "1300vh top",
+            end: "2025vh top",
+            scrub: true,
+        }
+    })
+
+    gsap.to(".feature_left", {
+        x: '500px',
+        ease: "power3.inOut",
+        scrollTrigger: {
+            trigger: ".web-content",
+            start: "880vh top",
+            end: "2025vh top",
+            scrub: true,
+        }
+    })
+
+    gsap.to(".feature_right", {
+        x: '-500px',
+        ease: "power3.inOut",
+        scrollTrigger: {
+            trigger: ".web-content",
+            start: "880vh top",
+            end: "2025vh top",
+            scrub: true,
+        }
+    })
+
+})
